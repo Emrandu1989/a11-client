276db6118180b1f258ff684721796b48e6d11d69
Emrandu1989 <emrandu1989@gmail.com>
2024-05-10 10:28:10 +0600

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,49 @@
-
+import { useLottie } from "lottie-react";
+import groovyWalkAnimation from "./../../assets/Animation - 1715313922697.json";
 
 const Login = () => {
+    const options = {
+        animationData: groovyWalkAnimation,
+        loop: true
+      };
+      const { View } = useLottie(options);
+
     return (
-        <div>
-              <h2>This is Login</h2>
+        <>
+
+
+             <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left ml-24">
+          
+         <span className="font-bold text-6xl text-gray-400">welcome</span> <span>{View}</span>
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body">
+         <h1 className="text-center font-bold text-3xl text-gray-400">Please Login</h1>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
         </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+        </>
     );
 };
 
